48ac8517784f6482005f5f4f95f967c7bd6c9120
Mun Joon Teo <teomunjoon101@gmail.com>
2023-02-12 00:15:42 +1100

    feat: Implemented autosave of game and loading a savegame

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,14 +1,42 @@
 import React, { useContext } from "react";
-import { GameContext } from "./GameContext";
+import { GameContext, IPlayer } from "./GameContext";
+
+interface Data {
+  wind: number;
+  round: number;
+  east: IPlayer;
+  south: IPlayer;
+  west: IPlayer;
+  north: IPlayer;
+}
 
 const Home: React.FC = () => {
-  const { setCurrentPage } = useContext(GameContext);
+  const { setEast, setSouth, setWest, setNorth, setWind, setRound, setCurrentPage } = useContext(GameContext);
+
+  const handleLoadGame = () => {
+    const data = localStorage.getItem("data");
+
+    if (!data) return;
+
+    const parsedData = JSON.parse(data) as Data;
+
+    const { east, south, west, north, wind, round } = parsedData;
+
+    setEast(east);
+    setSouth(south);
+    setWest(west);
+    setNorth(north);
+    setWind(wind);
+    setRound(round);
+
+    setCurrentPage("score");
+  }
 
   return (
     <div>
       Mahjong Points Counter
-      <button onClick={() => setCurrentPage("players")}>Start</button>
-      <button onClick={() => setCurrentPage("settings")}>Settings</button>
+      <button onClick={() => setCurrentPage("players")}>New Game</button>
+      <button onClick={handleLoadGame}>Load Game</button>
     </div>
   )
 }
--- src/Score.tsx
+++ src/Score.tsx
@@ -1,14 +1,20 @@
-import React, { useContext } from "react"
-import { GameContext, IPlayer } from "./GameContext"
+import React, { useContext, useEffect } from "react"
+import { GameContext, IPlayer, Seat } from "./GameContext"
 
 const Score: React.FC = () => {
   const { north, east, south, west, wind, round, setCurrentPage } = useContext(GameContext);
 
+  useEffect(() => {
+    const data = JSON.stringify({ wind, round, east, south, west, north });
+    localStorage.setItem("data", data);
+  }, [])
+
   const getWind = () => {
     const winds = ['East', 'South', 'West', 'North'];
-    
+
     return winds[wind];
   }
+
   const showPlayer = (player: IPlayer) => {
     return (
       <div>
@@ -18,6 +24,8 @@ const Score: React.FC = () => {
     )
   }
 
+  const isLastRound = wind === Seat.North && round == 4;
+
   return (
     <div>
       <div>Current Round: {getWind()} {round}</div>
@@ -25,7 +33,8 @@ const Score: React.FC = () => {
       {showPlayer(east)}
       {showPlayer(south)}
       {showPlayer(west)}
-      <button onClick={() => setCurrentPage("addScore")}>Add score</button>
+      <button disabled={isLastRound} onClick={() => setCurrentPage("addScore")}>Add score</button>
+      {isLastRound && <button onClick={() => setCurrentPage("home")}>Back to Home</button>}
     </div>
   )
 }
